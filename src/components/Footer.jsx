5791a56eb3d51781620ae1f067c3eee542a71165
Sila2 <silaikinci@hotmail.com>
2022-05-06 20:33:57 +0300

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        {/* <!-- Footer Starts --> */}
+<div className="footer text-center spacer">
+<p className="wowload flipInX">
+  <a href="#"><i class="fa-brands fa-facebook fa-2x"></i></a> 
+  <a href="#"><i class="fa-brands fa-instagram fa-2x"></i></a> 
+  <a href="#"><i class="fa-brands fa-twitter fa-2x"></i></a> 
+  <a href="#"><i class="fa-brands fa-linkedin fa-2x"></i></a> 
+</p>
+Copyright 2022
+</div>
+{/* <!-- # Footer Ends --> */}
+    </div>
+  )
+}
