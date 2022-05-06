0e9f302da1bce41ea447ddd444158f198c5c1356
Sila2 <silaikinci@hotmail.com>
2022-05-06 03:47:06 +0300

    Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        {/* <!-- Footer Starts --> */}
+<div class="footer text-center spacer">
+<p class="wowload flipInX"><a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="#"><i class="fa fa-dribbble fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a> <a href="#"><i class="fa fa-linkedin fa-2x"></i></a> </p>
+Copyright 2014 Cyrus Creative Studio. All rights reserved.
+</div>
+{/* <!-- # Footer Ends --> */}
+    </div>
+  )
+}
